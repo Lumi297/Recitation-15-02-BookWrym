54a050f7f088276fcbb38b6f7288b434e31849c7
Lumi297 <brle8381@colorado.edu>
2023-11-10 09:48:16 -0700

    adding basic testing, will likely be updated as more and more of the project is realized

--- Project/project_code/src/init_data/create.sql
+++ Project/project_code/src/init_data/create.sql
@@ -3,3 +3,6 @@ CREATE TABLE users(
     username VARCHAR(50) PRIMARY KEY,
     password CHAR(60) NOT NULL
 );
+
+-- following is for testing purposes only 
+INSERT INTO users (username, password) VALUES ('John doe', '12345'); 
